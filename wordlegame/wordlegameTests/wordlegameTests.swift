a3b2423c4f60af414a9f9ac3190e4bb350d0a4b4
Avlokita Singh <167293024+Avlokitas@users.noreply.github.com>
2025-06-17 12:45:03 +0530

    main game

---
+++ wordlegame/wordlegameTests/wordlegameTests.swift
@@ -0,0 +1,17 @@
+//
+//  wordlegameTests.swift
+//  wordlegameTests
+//
+//  Created by Avlokita on 16/06/25.
+//
+
+import Testing
+@testable import wordlegame
+
+struct wordlegameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
